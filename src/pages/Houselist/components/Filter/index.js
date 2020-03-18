11e650114ded32c40d0a80a58ef5e01efa93da22
YR <1106149466@qq.com>
2020-03-18 15:12:15 +0800

    openType 控制筛选框显示隐藏

--- src/pages/Houselist/components/Filter/index.js
+++ src/pages/Houselist/components/Filter/index.js
@@ -6,28 +6,70 @@ import FilterMore from '../FilterMore'
 
 import styles from './index.module.css'
 // 数据要写在父组件  四个都要判断
-const titleSelectedStatus = {
+const titleStatus = {
   area:false,
   mode:false,
-  price:true,
+  price:false,
   more:false
 }
 export default class Filter extends Component {
   state={
-    titleSelectedStatus
+    titleSelectedStatus:titleStatus,
+    openType:''
+  }
+  onTitltClick=(type)=>{
+    console.log('执行了filter',type);
+    this.setState({
+      titleSelectedStatus:{
+        ...titleStatus,
+        [type]:true  //属性是变量可以加中括号
+      },
+      openType:type
+    })
+  }
+  // 父  点击取消得函数
+  onCancel=()=>{
+    console.log('取消');
+    this.setState({
+      openType:''
+    })
+  }
+  // 父  点击确定得函数
+  onSave=()=>{
+    console.log('确认');
+    this.setState({
+      openType:''
+    })
+  }
+  renderPicker=()=>{
+    let {openType} = this.state
+    if(openType==='area'||openType==='mode'||openType==='price'){
+      return <FilterPicker onCancel={this.onCancel} onSave={this.onSave} />
+    }else{
+      return null
+    }
+  }
+  renderMask=()=>{
+    let {openType} = this.state
+    if(openType==='area'||openType==='mode'||openType==='price'){
+      return <div className={styles.mask} />
+    }else{
+      return null
+    }
   }
   render() {
     return (
       <div className={styles.root}>
         {/* 前三个菜单的遮罩层 */}
+        {this.renderMask()}
         {/* <div className={styles.mask} /> */}
 
         <div className={styles.content}>
           {/* 标题栏 */}
-          <FilterTitle titleSelectedStatus={this,this.state.titleSelectedStatus} />
+          <FilterTitle titleSelectedStatus={this,this.state.titleSelectedStatus} onTitltClick={this.onTitltClick} />
 
           {/* 前三个菜单对应的内容： */}
-          {/* <FilterPicker /> */}
+          {this.renderPicker()}
 
           {/* 最后一个菜单对应的内容： */}
           {/* <FilterMore /> */}
--- src/pages/Houselist/components/FilterTitle/index.js
+++ src/pages/Houselist/components/FilterTitle/index.js
@@ -14,13 +14,16 @@ const titleList = [
 // 1.class组件时this.props  2. 函数组件props接受传来得值
 export default function FilterTitle(props) {
   let {titleSelectedStatus} = props
-  console.log(titleSelectedStatus);
+  // console.log(titleSelectedStatus);
   return (
     <Flex align="center" className={styles.root}>
       {/* 循环生成四个标题 */}
       {titleList.map((item,index) => {
         let isSelect = titleSelectedStatus[item.type]
-        return <Flex.Item key={index}>
+        return <Flex.Item key={index} 
+        onClick={()=>{
+          props.onTitltClick(item.type)
+        }}>
         {/* 选中类名： selected */}
         <span className={[styles.dropdown, isSelect?styles.selected:''].join(' ')}>
           <span>{item.title}</span>
